27c53559debc2830729d12269a96110bc00ab44b
Urvashi-146 <sharmaurwashi146@gmail.com>
2025-07-12 10:22:41 +0530

    Update _init__.py

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+from dotenv import load_dotenv
+import os
+
 from .routes import main as main_routes
 
+load_dotenv()
+
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/rewear'
+
+
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/rewear')
+
 mongo = PyMongo(app)
 
 app.register_blueprint(main_routes)
